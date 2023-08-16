805f0b7cad803039460eb3b51886ea5d89172b25
Devon <devon.coates@totaleto.com>
2023-08-16 14:24:38 -0600

    setup connection to database and initial prompt

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SQLite;
+
+namespace HabitTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var running = true;
+
+
+            var _dataBaseString = "Data Source=HabitTracker.db";
+            using (var sqlConn = new SQLiteConnection(_dataBaseString))
+            {
+                sqlConn.Open();
+                var tblCommand = sqlConn.CreateCommand();
+                tblCommand.CommandText = @"CREATE TABLE IF NOT EXISTS habits(
+                                           Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                           name TEXT,
+                                           quantity INTERGER
+                                           )";
+                tblCommand.ExecuteNonQuery();
+                sqlConn.Close();
+            }
+            while (running)
+            {
+                Console.WriteLine
+                    (
+@"What would you like to do?
+
+Type 0 to Close The App.
+Type 1 to View All Records.
+Type 2 to Insert a Record.
+Type 3 to Delete a Record.
+Type 4 to Update a Record.
+---------------------------
+                    ");
+                var response = Console.ReadLine();
+                switch (response)
+                {
+                    case "0":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Response");
+                        break;
+                }
+            }
+        }
+    }
+}
+
