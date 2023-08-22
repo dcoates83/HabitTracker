0a0b4e99f1282c0c60cb3f8954d52649d15d9d50
Devon <devon.coates@totaleto.com>
2023-08-22 14:56:03 -0600

    added updating

--- HabitTracker/Habit.cs
+++ HabitTracker/Habit.cs
@@ -4,8 +4,7 @@
     internal class Habit
     {
         public int Id { get; set; }
-
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Quantity { get; set; }
 
     }
--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -13,7 +13,9 @@ namespace HabitTracker
             {
                 Console.WriteLine
                     (
-@"What would you like to do?
+@"
+---------------------------
+What would you like to do?
 
 Type 0 to Close The App.
 Type 1 to View All Records.
@@ -36,12 +38,87 @@ Type 4 to Update a Record.
                         var habit = Console.ReadLine();
                         Console.WriteLine("What is the quantity");
                         var quantityString = Console.ReadLine();
+
+                        if (String.IsNullOrWhiteSpace(habit) || String.IsNullOrWhiteSpace(quantityString))
+                        {
+                            Console.WriteLine("Invalid Response");
+                            break;
+                        }
                         var quantity = Convert.ToInt32(quantityString);
                         SQLConnection.InsertRecord(habit, quantity, _dataBaseString);
                         break;
                     case "3":
+                        Console.WriteLine("What habit do you want to Delete");
+                        var _deleteHabit = Console.ReadLine();
+
+                        if (String.IsNullOrWhiteSpace(_deleteHabit))
+                        {
+                            Console.WriteLine("Invalid Response");
+                            break;
+                        }
+
+                        SQLConnection.DeleteRecord(_deleteHabit, _dataBaseString);
                         break;
                     case "4":
+
+                        Console.WriteLine("Please enter the ID of the habit you want to update");
+                        var _updateID = Console.ReadLine();
+                        int _id;
+
+                        bool isId = int.TryParse(_updateID, out _id);
+
+
+                        if (isId)
+                        {
+
+
+                            Console.WriteLine("Do you want to update the quantity or the habit?");
+                            var _whatToUpdate = Console.ReadLine();
+
+                            if (_whatToUpdate == "habit")
+                            {
+                                Console.WriteLine("Please enter the new habit");
+                                var _reqHabit = Console.ReadLine();
+
+                                if (_reqHabit != null)
+                                {
+                                    SQLConnection.UpdateRecord(_id, _dataBaseString, _reqHabit);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Please enter a valid response");
+                                }
+                            }
+                            else if (_whatToUpdate == "quantity")
+                            {
+                                Console.WriteLine("Please enter the new Value");
+                                var _reqQuantity = Console.ReadLine();
+                                int _newQuantity;
+                                var _canConvert = int.TryParse(_reqQuantity, out _newQuantity);
+                                if (_canConvert)
+                                {
+                                    SQLConnection.UpdateRecord(_id, _dataBaseString, _newQuantity);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error converting the number");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid Type");
+                            }
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter a valid ID");
+                        }
+
+
+
+
                         break;
                     default:
                         Console.WriteLine("Invalid Response");
@@ -104,9 +181,6 @@ Type 4 to Update a Record.
                         var _name = (string)reader.GetValue(1);
                         var _quantity = reader.IsDBNull(2) ? 0 : reader.GetFieldValue<long>(2);
 
-
-                        Console.WriteLine(_id.ToString(), _name.ToString(), _quantity.ToString());
-
                         tableData.Add(new Habit
                         {
                             Id = (int)_id,
@@ -125,6 +199,67 @@ Type 4 to Update a Record.
                     Console.WriteLine($"ID:{row.Id} - Name:{row.Name} - Quantity:{row.Quantity}");
                 }
                 Console.WriteLine("-----------------------------------\n");
+                sqlConn.Close();
+            }
+        }
+        public static void DeleteRecord(string habit, string conn)
+        {
+            using (var sqlConn = new SQLiteConnection(conn))
+            {
+                sqlConn.Open();
+                var tblCommand = sqlConn.CreateCommand();
+                tblCommand.CommandText =
+$@"
+DELETE FROM habits
+WHERE habits.name = '{habit}';
+";
+                tblCommand.ExecuteNonQuery();
+                sqlConn.Close();
+            }
+        }
+        public static void UpdateRecord(int id, string conn, string? habit)
+        {
+            using (var sqlConn = new SQLiteConnection(conn))
+            {
+
+                sqlConn.Open();
+                var tblCommand = sqlConn.CreateCommand();
+
+
+                if (!String.IsNullOrWhiteSpace(habit))
+                {
+                    tblCommand.CommandText =
+                                $@"
+UPDATE habits
+SET name = '{habit}'
+WHERE habits.Id  = {id};
+";
+                    tblCommand.ExecuteNonQuery();
+                }
+
+                sqlConn.Close();
+            }
+        }
+        public static void UpdateRecord(int id, string conn, int? quantity)
+        {
+            using (var sqlConn = new SQLiteConnection(conn))
+            {
+
+                sqlConn.Open();
+                var tblCommand = sqlConn.CreateCommand();
+
+                if (quantity != null)
+                {
+                    tblCommand.CommandText =
+                 $@"
+UPDATE habits
+SET quantity = {quantity}
+WHERE habits.Id  = {id};
+";
+                    tblCommand.ExecuteNonQuery();
+                }
+
+
                 sqlConn.Close();
             }
         }
