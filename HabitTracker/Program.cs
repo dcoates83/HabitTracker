f15d39d216ab8aff53f918db05877c503764d7dc
Devon <devon.coates@totaleto.com>
2023-08-22 15:14:08 -0600

    cleanup

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Data.SQLite;
-
-namespace HabitTracker
+﻿namespace HabitTracker
 {
     class Program
     {
@@ -70,8 +68,6 @@ Type 4 to Update a Record.
 
                         if (isId)
                         {
-
-
                             Console.WriteLine("Do you want to update the quantity or the habit?");
                             var _whatToUpdate = Console.ReadLine();
 
@@ -128,141 +124,6 @@ Type 4 to Update a Record.
         }
 
 
-    }
-    public class SQLConnection
-    {
-
-        public static void CreateTable(string conn)
-        {
-
-            using (var sqlConn = new SQLiteConnection(conn))
-            {
-                sqlConn.Open();
-                var tblCommand = sqlConn.CreateCommand();
-                tblCommand.CommandText = @"CREATE TABLE IF NOT EXISTS habits(
-                                           Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                           name TEXT,
-                                           quantity INTERGER
-                                           )";
-                tblCommand.ExecuteNonQuery();
-                sqlConn.Close();
-            }
-        }
-        public static void InsertRecord(string habit, int quantity, string conn)
-        {
-            using (var sqlConn = new SQLiteConnection(conn))
-            {
-
-                sqlConn.Open();
-                var tblCommand = sqlConn.CreateCommand();
-                tblCommand.CommandText =
-                    $@"INSERT INTO habits
-                       (name, quantity) VALUES('{habit}',{quantity})";
-                tblCommand.ExecuteNonQuery();
-                sqlConn.Close();
-            }
-        }
-        public static void ViewAllRecords(string conn)
-        {
-            using (var sqlConn = new SQLiteConnection(conn))
-            {
-                sqlConn.Open();
-                var tblCommand = sqlConn.CreateCommand();
-                tblCommand.CommandText = "SELECT * FROM Habits";
-                tblCommand.ExecuteNonQuery();
-                var reader = tblCommand.ExecuteReader();
-                List<Habit> tableData = new();
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-
-                        var _id = (long)reader.GetValue(0);
-                        var _name = (string)reader.GetValue(1);
-                        var _quantity = reader.IsDBNull(2) ? 0 : reader.GetFieldValue<long>(2);
-
-                        tableData.Add(new Habit
-                        {
-                            Id = (int)_id,
-                            Name = String.IsNullOrWhiteSpace(_name) ? "" : _name,
-                            Quantity = (int)_quantity
-                        });
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("No Rows Found");
-                }
-                Console.WriteLine("-----------------------------------\n");
-                foreach (var row in tableData)
-                {
-                    Console.WriteLine($"ID:{row.Id} - Name:{row.Name} - Quantity:{row.Quantity}");
-                }
-                Console.WriteLine("-----------------------------------\n");
-                sqlConn.Close();
-            }
-        }
-        public static void DeleteRecord(string habit, string conn)
-        {
-            using (var sqlConn = new SQLiteConnection(conn))
-            {
-                sqlConn.Open();
-                var tblCommand = sqlConn.CreateCommand();
-                tblCommand.CommandText =
-$@"
-DELETE FROM habits
-WHERE habits.name = '{habit}';
-";
-                tblCommand.ExecuteNonQuery();
-                sqlConn.Close();
-            }
-        }
-        public static void UpdateRecord(int id, string conn, string? habit)
-        {
-            using (var sqlConn = new SQLiteConnection(conn))
-            {
-
-                sqlConn.Open();
-                var tblCommand = sqlConn.CreateCommand();
-
-
-                if (!String.IsNullOrWhiteSpace(habit))
-                {
-                    tblCommand.CommandText =
-                                $@"
-UPDATE habits
-SET name = '{habit}'
-WHERE habits.Id  = {id};
-";
-                    tblCommand.ExecuteNonQuery();
-                }
-
-                sqlConn.Close();
-            }
-        }
-        public static void UpdateRecord(int id, string conn, int? quantity)
-        {
-            using (var sqlConn = new SQLiteConnection(conn))
-            {
-
-                sqlConn.Open();
-                var tblCommand = sqlConn.CreateCommand();
-
-                if (quantity != null)
-                {
-                    tblCommand.CommandText =
-                 $@"
-UPDATE habits
-SET quantity = {quantity}
-WHERE habits.Id  = {id};
-";
-                    tblCommand.ExecuteNonQuery();
-                }
-
-
-                sqlConn.Close();
-            }
-        }
     }
 }
 
